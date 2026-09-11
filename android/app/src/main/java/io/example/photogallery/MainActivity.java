70c9c1e093cb8b56a06443f96acd95b817236433
Levdotdev <brito.lancekianne@minsu.edu.ph>
2026-09-11 19:41:49 +0800

    build

---
+++ android/app/src/main/java/io/example/photogallery/MainActivity.java
@@ -0,0 +1,5 @@
+package io.example.photogallery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'task_manager',
+  appId: 'io.example.photogallery',
+  appName: 'photo-gallery',
   webDir: 'dist'
 };
 
